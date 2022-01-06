49564805c8d469927539a242d34c07b5a5ea8136
lzlocki <lzlocki@poczta.fm>
2022-01-06 17:09:46 +0100

    Call selection sort class

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DataStructuresAndAlgorithms_C;
+
+// array for selection purpose
+int[] Array = new int [] {-11, 12, -42, 0, 1, 90, 68, 6, -9};
+
+void ShowArray(int[] array)
+{
+    string arrayString = "";
+    for(int i = 0; i < array.Length; i++)
+    {
+        arrayString += " " + array[i];
+    }
+    Console.WriteLine("{0}", arrayString);
+}
+
+// *** Selection Sort Part ***
+Console.WriteLine("Selection Algorithms - Selection sort:");
+Console.WriteLine("Before sort:");
+ShowArray(Array);
+int[] SortedArray = SelectionSort.StartSelectionSort(Array);
+Console.WriteLine("Selection sort result:");
+ShowArray(SortedArray);
+
+
+// *** Selection Sort Part ***
+ 
